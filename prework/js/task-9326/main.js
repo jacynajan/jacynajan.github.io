0cc34a5cdb1cb75631b7a2efce5a5d0547436d87
jacynajan <jacynajan@gmail.com>
2018-06-05 20:41:05 +0200

    add function

---
+++ prework/js/task-9326/main.js
@@ -0,0 +1,12 @@
+var num1 = 2;
+var num2 = 5;
+
+function bigNum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+console.log(bigNum(num1, num2));
