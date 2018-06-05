01d66fed25dd375184548ef72de0bfdd5ca87e79
jacynajan <jacynajan@gmail.com>
2018-06-05 11:40:54 +0200

    add function

---
+++ prework/js/task-9323/main.js
@@ -0,0 +1,9 @@
+var liczbaPierwsza = 1;
+var liczbaDruga = 3;
+
+function sum(liczbaPierwsza, liczbaDruga) {
+    var suma = liczbaPierwsza + liczbaDruga;
+    return suma;
+}
+
+sum(liczbaPierwsza, liczbaDruga);
