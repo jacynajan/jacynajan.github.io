129d01eb857b328197af15c2ebc217962e321c2b
jacynajan <jacynajan@gmail.com>
2018-06-06 20:08:04 +0200

    add func

---
+++ prework/js/task-9334/main.js
@@ -0,0 +1,10 @@
+var name = document.getElementById('name');
+var second = document.getElementById('second');
+var third = document.getElementById('third');
+
+
+function func(){
+    name1.innerHTML = first.value;
+    surname.innerHTML = second.value
+    phone.innerHTML = third.value
+}
