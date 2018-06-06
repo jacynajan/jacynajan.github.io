9f78bf33e5be19507150e755c19e5f2919773661
jacynajan <jacynajan@gmail.com>
2018-06-06 16:16:49 +0200

    add function

---
+++ prework/js/task-9329/main.js
@@ -0,0 +1,8 @@
+function func(num) {
+    if (num >= 100 && num <= 200) {
+        console.log('Liczba znajduje się w przedziale');
+    } else {
+        console.log('Liczba nie znajduje się w przedziale');
+    }
+}
+func(19);
